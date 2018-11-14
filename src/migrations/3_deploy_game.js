999992dafbc5a28cb03f3dec72ef7f2ee061566b
alexstep <alex.step.kellas@gmail.com>
2018-11-14 15:58:41 +0300

    last contracts and migrations

---
+++ src/migrations/3_deploy_game.js
@@ -0,0 +1,14 @@
+const ERC20     = artifacts.require('../contracts/token/ERC20.sol')
+const Platform  = artifacts.require('../contracts/core/Platform.sol')
+const Utils     = artifacts.require('../contracts/lib/Utils.sol')
+const Signidice = artifacts.require('../contracts/Signidice/Signidice.sol')
+
+const Engine   = artifacts.require('../contracts/gameEngine/GameEngine.sol')
+const Game     = artifacts.require('../contracts/game/myDappGame.sol')
+
+module.exports = async (deployer) => {
+    await deployer.link(Utils, Game)
+    await deployer.deploy(Engine, Signidice.address)
+    await deployer.deploy(Game, Engine.address, ERC20.address, Platform.address, Signidice.address)
+    console.log('>>> Deploy Game contracts - complete <<<')
+}
