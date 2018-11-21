d61d5a7d5c0ce27f4ddf00725f107ef34d113b31
alexstep <alex.step.kellas@gmail.com>
2018-11-21 17:57:28 +0300

    refactor(dc-protocol): new contracts version
    New contracts, migrations, gameABI
    
    BREAKING CHANGE: openChannel, state - new arguments , new game ABI, dc-core, dc-configs now need to
    be change

--- src/migrations/3_deploy_game.js
+++ src/migrations/3_deploy_game.js
@@ -1,3 +1,11 @@
+<<<<<<< HEAD
+const ERC20       = artifacts.require('../contracts/token/ERC20.sol')
+const Platform    = artifacts.require('../contracts/platform/Platform.sol')
+const Utils       = artifacts.require('../contracts/library/Utils.sol')
+const Signidice   = artifacts.require('../contracts/Signidice/Signidice.sol')
+const GameEngine  = artifacts.require('../contracts/gameEngine/GameEngine.sol')
+const Game        = artifacts.require('../contracts/game/MyDappGame.sol')
+=======
 const ERC20     = artifacts.require('../contracts/token/ERC20.sol')
 const Platform  = artifacts.require('../contracts/platform/Platform.sol')
 const Utils     = artifacts.require('../contracts/lib/Utils.sol')
@@ -5,10 +13,13 @@ const Signidice = artifacts.require('../contracts/Signidice/Signidice.sol')
 
 const Engine   = artifacts.require('../contracts/gameEngine/GameEngine.sol')
 const Game     = artifacts.require('../contracts/game/myDappGame.sol')
+>>>>>>> development
 
 module.exports = async (deployer) => {
-    await deployer.link(Utils, Game)
-    await deployer.deploy(Engine, Signidice.address)
-    await deployer.deploy(Game, Engine.address, ERC20.address, Platform.address, Signidice.address)
-    console.log('>>> Deploy Game contracts - complete <<<')
+  await deployer.link(Utils, Game)
+  await deployer.deploy(GameEngine, Signidice.address)
+  await deployer.deploy(Game, GameEngine.address, ERC20.address, Platform.address, Signidice.address)
+  const GameContract = await Game.deployed()
+  const name = await GameContract.getName.call()
+  console.log(`>>> Deploy ${name} - complete <<<`)
 }
