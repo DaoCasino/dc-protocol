dadbe5f07dcf96ca6d2cabd8561a87ebf3375699
Your Name <you@example.com>
2018-11-22 18:04:11 +0300

    fix: del 42 migrate krasotu; and add cross env to package.json command

--- src/migrations/3_deploy_game.js
+++ src/migrations/3_deploy_game.js
@@ -1,18 +1,23 @@
-const ERC20       = artifacts.require('../contracts/token/ERC20.sol')
-const Platform    = artifacts.require('../contracts/platform/Platform.sol')
-const Utils       = artifacts.require('../contracts/library/Utils.sol')
-const Signidice   = artifacts.require('../contracts/Signidice/Signidice.sol')
-const GameEngine  = artifacts.require('../contracts/gameEngine/GameEngine.sol')
-const Game        = artifacts.require('../contracts/game/MyDappGame.sol')
+const ERC20 = artifacts.require("../contracts/token/ERC20.sol")
+const Platform = artifacts.require("../contracts/platform/Platform.sol")
+const Utils = artifacts.require("../contracts/library/Utils.sol")
+const Signidice = artifacts.require("../contracts/Signidice/Signidice.sol")
+const GameEngine = artifacts.require("../contracts/gameEngine/GameEngine.sol")
+const Game = artifacts.require("../contracts/game/MyDappGame.sol")
 
-module.exports = async (deployer) => {
+module.exports = async deployer => {
   await deployer.link(Utils, Game)
   await deployer.deploy(GameEngine, Signidice.address)
-  await deployer.deploy(Game, GameEngine.address, ERC20.address, Platform.address, Signidice.address)
-  const GameContract = await Game.deployed()
-  const name = await GameContract.getName.call()
+  await deployer.deploy(
+    Game,
+    GameEngine.address,
+    ERC20.address,
+    Platform.address,
+    Signidice.address
+  )
+  // const GameContract = await Game.deployed()
+  // const name = await GameContract.getName.call()
   console.log(`
-        >>> Deploy ${name} - complete <<<
-        address: ${Game.address}
+        >>> Deploy complete <<<
   `)
 }
